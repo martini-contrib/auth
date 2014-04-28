4cf641a9317dc9a15625b16a63fa5620e5dc1896
Attila Oláh <attilaolah@gmail.com>
2014-04-28 17:11:14 +0700

    implement BasicFunc (closes #2 in martini-contrib/auth)

--- basic.go
+++ basic.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"encoding/base64"
 	"net/http"
+	"strings"
 )
 
 // Basic returns a Handler that authenticates via Basic Auth. Writes a http.StatusUnauthorized
@@ -12,8 +13,33 @@ func Basic(username string, password string) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		auth := req.Header.Get("Authorization")
 		if !SecureCompare(auth, "Basic "+siteAuth) {
-			res.Header().Set("WWW-Authenticate", "Basic realm=\"Authorization Required\"")
-			http.Error(res, "Not Authorized", http.StatusUnauthorized)
+			unauthorized(res, "Authorization Required")
 		}
 	}
+}
+
+// BasicFunc returns a Handler that authenticates via Basic Auth using the provided function.
+// The function should return true for a valid username/password combination.
+func BasicFunc(authfn func(string, string) bool) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		auth := req.Header.Get("Authorization")
+		if len(auth) < 6 || auth[:6] != "Basic " {
+			unauthorized(res, "Authorization Required")
+			return
+		}
+		b, err := base64.StdEncoding.DecodeString(auth[6:])
+		if err != nil {
+			unauthorized(res, "Authorization Required")
+			return
+		}
+		tokens := strings.SplitN(string(b), ":", 2)
+		if len(tokens) != 2 || !authfn(tokens[0], tokens[1]) {
+			unauthorized(res, "Authorization Required")
+		}
+	}
+}
+
+func unauthorized(res http.ResponseWriter, realm string) {
+	res.Header().Set("WWW-Authenticate", "Basic realm=\"" + realm + "\"")
+	http.Error(res, "Not Authorized", http.StatusUnauthorized)
 }
--- basic_test.go
+++ basic_test.go
@@ -43,3 +43,57 @@ func Test_BasicAuth(t *testing.T) {
 		t.Error("Auth failed, got: ", recorder.Body.String())
 	}
 }
+
+func Test_BasicFuncAuth(t *testing.T) {
+	for auth, valid := range map[string]bool{
+		"foo:spam":       true,
+		"bar:spam":       true,
+		"foo:eggs":       false,
+		"bar:eggs":       false,
+		"foo:spam:extra": false,
+		"dummy:":         false,
+		"dummy":          false,
+		"":               false,
+	} {
+		recorder := httptest.NewRecorder()
+		encoded := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+
+		m := martini.New()
+		m.Use(BasicFunc(func(username, password string) bool {
+			return (username == "foo" || username == "bar") && password == "spam"
+		}))
+		m.Use(func(res http.ResponseWriter, req *http.Request) {
+			res.Write([]byte("hello"))
+		})
+
+		r, _ := http.NewRequest("GET", "foo", nil)
+
+		m.ServeHTTP(recorder, r)
+
+		if recorder.Code != 401 {
+			t.Error("Response not 401, params:", auth)
+		}
+
+		if recorder.Body.String() == "hello" {
+			t.Error("Auth block failed, params:", auth)
+		}
+
+		recorder = httptest.NewRecorder()
+		r.Header.Set("Authorization", encoded)
+		m.ServeHTTP(recorder, r)
+
+		if valid && recorder.Code == 401 {
+			t.Error("Response is 401, params:", auth)
+		}
+		if !valid && recorder.Code != 401 {
+			t.Error("Response not 401, params:", auth)
+		}
+
+		if valid && recorder.Body.String() != "hello" {
+			t.Error("Auth failed, got: ", recorder.Body.String(), "params:", auth)
+		}
+		if !valid && recorder.Body.String() == "hello" {
+			t.Error("Auth block failed, params:", auth)
+		}
+	}
+}
